33712b8d690374bd659b5f87739673b2e9502ec5
yıldız <mesel4461@gmail.com>
2024-02-02 01:05:12 +0300

    finish all

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 function generateComputerChoice() {
@@ -24,4 +26,29 @@ function generateComputerChoice() {
     computerChoice = "paper";
   }
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw!";
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "You win!";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "You lost!";
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "You win!";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "You lost!";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "You win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "You lost!";
+  }
+  resultDisplay.innerHTML = result;
 }
